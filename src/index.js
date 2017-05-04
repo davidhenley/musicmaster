341aea1808b3cfc02ea78b0172990dac79d034ea
David Henley <henleydr@gmail.com>
2017-05-04 14:12:20 -0500

    FormGroup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
+import './style.css';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
-const App = () => {
-  return (
-    <div>Hi</div>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <div className="App container">
+        <div className="Title">Music Master</div>
+        <FormGroup>
+          <InputGroup>
+            <FormControl
+              type="text"
+              placeholder="Search for an Artist"
+            />
+            <InputGroup.Addon>
+              <Glyphicon glyph="search" />
+            </InputGroup.Addon>
+          </InputGroup>
+        </FormGroup>
+        <div className="Profile">
+          <div>Artist Picture</div>
+          <div>Artist Name</div>
+        </div>
+        <div className="Gallery">
+          Gallery
+        </div>
+      </div>
+    );
+  }
+}
 
 render(<App />, document.querySelector('#root'));
